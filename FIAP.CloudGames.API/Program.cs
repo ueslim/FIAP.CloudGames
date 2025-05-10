0e9f08e6a8a06312ec3abda7c36c8699bdc71960
ueslim <107502335+ueslim@users.noreply.github.com>
2025-05-10 19:38:36 -0300

    Add project files.

---
+++ FIAP.CloudGames.API/Program.cs
@@ -0,0 +1,32 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Aqui registrar os serviços (ex: DbContext, Repositories, etc)
+// builder.Services.AddDbContext<SeuDbContext>(...);
+// builder.Services.AddScoped<ISeuServico, SeuServico>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Middleware de tratamento global de erros (pode implementar depois)
+// app.UseMiddleware<SeuMiddlewareDeErro>();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/health", () => Results.Ok("API funcionando!"));
+
+app.Run();
---
+++ FIAP.CloudGames.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FIAP.CloudGames.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
