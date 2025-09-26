242c17a75ec9d5d455cd4fdda5b56462ee90b723
Denis Silva <denisvr01@gmail.com>
2025-09-26 19:25:35 +0100

    removed old code

--- FIAP.CloudGames.Application/Interfaces/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using FIAP.CloudGames.Domain.Entities;
-
-namespace FIAP.CloudGames.Application.Interfaces
-{
-    public interface ITokenService
-    {
-        string GenerateToken(User user);
-    }
-}
